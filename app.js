33231697d470be9e1827aedde0ea975d73de0e51
amadi5892 <66728197+amadi5892@users.noreply.github.com>
2020-07-02 22:38:52 -0400

    Created racetrack via CSS

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
-$(document).keypress("a", function() {
-    $(".box1").animate({
-        "left": "50%"
-    }, 20000)
+$(function() {
+    
+    var num = 4300 - window.innerWidth;
+    
+    $(document).keypress("a", function() {
+        $(".container").animate({
+            "left": -num
+        }, 2000)
+        // alert(num)
+    });
 
-});
 
-// $(".box2").animate({
-//     "left": "3000px"
-// }, 7500);
+})
+
+
+
+
+
+
+// $(document).keypress("a", function() {
+//     $(".container").animate({
+//         "left": -num
+//     }, 2000)
+
+// });
+
+$(".box2").animate({
+    "left": window.innerWidth - 80
+}, 2000);
 
 
 // $(document).keydown(function(event) {
 //     alert(event.keyCode);
 // })
+
+// var num = 4300 - window.innerWidth;
